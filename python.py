0d1edef9539e6c31a4552abb9b04e10c88316791
Mahmood_Reda <mr4141705@gmail.com>
2025-07-21 18:15:32 +0300

    ِِAdd example using the print function .

---
+++ python.py
@@ -0,0 +1,6 @@
+#!/user/bin/python
+print("Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com")
+
+# Full Name: Mahmoud Reda
+# Birth Date: 2001/01/15
+# E-mail: mr4141705@gmail.com
