e404b40e31b850246def2643d23c426dad72f401
Mahmood_Reda <mr4141705@gmail.com>
2025-07-21 18:51:20 +0300

    add example showing dynamic typing in Python .

--- python.py
+++ python.py
@@ -1,6 +1,15 @@
 #!/user/bin/python
-print("Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com")
 
-# Full Name: Mahmoud Reda
-# Birth Date: 2001/01/15
-# E-mail: mr4141705@gmail.com
+Value = 10
+print(Value)
+print(type(Value))
+Value = "Reda"
+print(Value)
+print(type(Value))
+
+
+
+# 10
+# <class 'int'>
+# Reda
+# <class 'str'>
