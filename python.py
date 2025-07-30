c50335242cb48ef8622cc087dec2de609a692676
Mahmood_Reda <mr4141705@gmail.com>
2025-07-30 20:10:12 +0300

    Completed task: Created website launcher module .

--- python.py
+++ python.py
@@ -1,79 +1,27 @@
 #!/usr/bin/python
 
-# Function to display a simple message
-def display_message():
-    print("Reda")
+import src.Cal as firelink
 
-display_message()
+while True:
+    print("\nChoose a website to open:")
+    print("1. Facebook")
+    print("2. YouTube")
+    print("3. GitHub")
+    print("4. LinkedIn (Mahmood Reda)")
+    print("0. Exit")
 
-# Function to calculate the product of two numbers
-def multiply_numbers(a, b):
-    return a * b
+    choice = input("Enter your choice: ")
 
-print(multiply_numbers(5, 9))
-
-# Function to display three keyword arguments
-def display_arguments(first, second, third):
-    print("1", first)
-    print("2", second)
-    print("3", third)
-
-display_arguments(first="e", third=5, second=quit)
-
-# Function to display contents of a list
-def print_list(items):
-    unused_variable = 5  # Can be removed if not needed
-    print(items)
-
-sample_list = ["hg", "25"]
-print_list(sample_list)
-
-# Function that accepts any number of positional arguments
-def show_args(*args):
-    print(type(args))  # Shows it's a tuple
-    print(args)
-
-show_args("s", "e")
-
-# Will print 'None' because print_list doesn't return anything
-print(print_list(sample_list))
-
-# Function that accepts any number of keyword arguments
-def show_kwargs(**kwargs):
-    print(kwargs)
-
-show_kwargs(language="Python")
-
-# Demonstration of variable scope (local variable)
-global_value = 8
-
-def demonstrate_local_scope():
-    local_value = 9
-    print(local_value)
-    print(id(local_value))
-
-demonstrate_local_scope()
-print(global_value)
-print(id(global_value))
-
-# Using 'global' keyword to change global variable
-global_value = 8
-
-def modify_global_variable():
-    global global_value
-    global_value = 9
-    print(global_value)
-    print(id(global_value))
-
-modify_global_variable()
-print(global_value)
-print(id(global_value))
-
-# Importing and using a function from a custom module
-import src.Cal as Calculator
-import importlib
-
-print(Calculator.sum(5, 3))  # Assumes sum(a, b) is defined in src/Cal.py
-
-# Dynamically importing a module (Google Text-to-Speech)
-print(importlib.import_module("gtts"))
+    if choice == "1":
+        firelink.firefox(firelink.facebook_link)
+    elif choice == "2":
+        firelink.firefox(firelink.youtube_link)
+    elif choice == "3":
+        firelink.firefox(firelink.github_link)
+    elif choice == "4":
+        firelink.firefox(firelink.linkedin_link)
+    elif choice == "0":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
--- src/Cal.py
+++ src/Cal.py
@@ -1,3 +1,13 @@
-def sum(a,b):
-    return a+b
-   
+# firelink.py
+# firelink.py
+import webbrowser
+
+# Favorite websites
+facebook_link = "https://www.facebook.com"
+youtube_link = "https://www.youtube.com"
+github_link = "https://github.com"
+linkedin_link = "https://www.linkedin.com/in/mahmood-reda/"
+
+# Function to open a website in the default browser
+def firefox(url):
+    webbrowser.open(url)
