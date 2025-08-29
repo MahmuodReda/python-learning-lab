d5622c9184635d816b085a432b1f57d19b0fcdd6
Mahmood_Reda <mr4141705@gmail.com>
2025-08-29 19:28:27 +0300

    Add simple threading example with 2 threads.

--- python.py
+++ python.py
@@ -1,6 +1,27 @@
 #!/user/bin/python
-print("Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com")
+import threading   # Import threading library to create threads
+from time import sleep   # Import sleep function to pause execution
 
-# Full Name: Mahmoud Reda
-# Birth Date: 2001/01/15
-# E-mail: mr4141705@gmail.com
+# Task 1 function
+def task1():
+    while True:  # Infinite loop
+        print("Task 1")   # Print message
+        sleep(1)          # Pause for 1 second
+
+# Task 2 function
+def task2():
+    while True:  # Infinite loop
+        print("Task 2")   # Print message
+        sleep(1)          # Pause for 1 second
+
+# Create and start threads
+t1 = threading.Thread(target=task1)   # Create thread for task1
+t2 = threading.Thread(target=task2)   # Create thread for task2
+
+t1.start()  # Start thread 1
+t2.start()  # Start thread 2
+
+# Main thread work
+while True:  
+    print("Main Thread")   # Print message for main thread
+    sleep(1)               # Pause for 1 second
